a27da3d2d356fe0688b2371c04513a9fc802362f
Shivkumar Shivaji <sshivaji@gmail.com>
2012-11-12 09:50:23 -0800

    Cleaned up debug

--- ctg_helper.c
+++ ctg_helper.c
@@ -245,7 +245,9 @@ void ctg_iterate_page(int page_index) {
     fseek(ctg_file, 4096 * (page_index + 1), SEEK_SET);
     if (!fread(buf, 1, 4096, ctg_file)) return;
     int num_positions = (buf[0] << 8) + buf[1];
-    //printf("found %d positions\n", num_positions);
+    #ifdef DEBUG
+        printf("found %d positions\n", num_positions);
+    #endif
 
     // Just scan through the list until we find a matching signature.
     int pos = 4;
@@ -272,16 +274,16 @@ void ctg_iterate_page(int page_index) {
         for (int j = 1; j < entry_size; ++j) entry->moves[j - 1] = buf[pos + j];
         entry->num_moves = (entry_size - 1) / 2;
 
-        #ifdef DEBUG
-            printf("entry->num_moves: %d\n", entry->num_moves);
-        #endif
+//        #ifdef DEBUG
+//            printf("entry->num_moves: %d\n", entry->num_moves);
+//        #endif
 
         pos += entry_size;
         entry->total = read_24(buf, pos);
 
-        #ifdef DEBUG
-            printf("entry->total: %d\n", entry->total);
-        #endif
+//        #ifdef DEBUG
+//            printf("entry->total: %d\n", entry->total);
+//        #endif
 
         pos += 3;
         entry->losses = read_24(buf, pos);
